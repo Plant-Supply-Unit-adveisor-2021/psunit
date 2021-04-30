c412d29aed7962ea5efdd80fb8bd6063376007a5
MaxTechCode <maxtech@t-online.de>
2021-04-30 18:32:14 +0200

    display working

--- sensors/test.py
+++ sensors/test.py
@@ -1,6 +1,8 @@
 from sensors.camera import make_picture
 from sensors.pump import pump_water
+from sensors.display import display_test
 
 def test():
     print("Sensors Test")
     # pump_water(150)
+    display_test()
