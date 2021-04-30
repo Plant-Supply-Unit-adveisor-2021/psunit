15a572cf43b6279f1d663863582094468d3d9a11
MaxTechCode <maxtech@t-online.de>
2021-04-30 19:36:45 +0200

    added rotary encoder setup

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,5 +1,8 @@
+from sensors.rotary import Rotary
+
 """
-configuration of pins
+configuration of pins and co
 """
 
 GPIO_PUMP = 21
+ROTARY = Rotary(pin_clk=27, pin_dt=22, pin_sw=17)
--- sensors/test.py
+++ sensors/test.py
@@ -1,3 +1,6 @@
+from signal import pause
+
+from sensors import ROTARY
 from sensors.camera import make_picture
 from sensors.pump import pump_water
 from sensors.display import display_test
@@ -5,4 +8,8 @@ from sensors.display import display_test
 def test():
     print("Sensors Test")
     # pump_water(150)
-    display_test()
+    # display_test()
+    ROTARY.set_clk(lambda : print("clk"))
+    ROTARY.set_cclk(lambda : print("cclk"))
+    ROTARY.set_push(lambda : print("push"))
+    pause()
