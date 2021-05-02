15e4d4207fc54f5f503c83914af0b8bb3917bcb0
Minawi2002 <mahmoudelminawi2@hotmail.com>
2021-05-02 11:01:52 +0100

    requirement added for DHT22

--- sensors/DHT22.py
+++ sensors/DHT22.py
@@ -1,10 +1,11 @@
 import Adafruit_DHT
+from settings import GPIO_DHT22
+
 
 DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
 
 def measure_temp_ahum():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_DHT22)
 
     if humidity is not None and temperature is not None:
         return (temperature, humidity)
--- settings.py
+++ settings.py
@@ -7,7 +7,11 @@
 # Pump
 GPIO_PUMP = 21
 
+# DHT22
+GPIO_DHT22 = 25
+
 # Rotary Encoder
 GPIO_ROT_CLK = 27
 GPIO_ROT_DT = 22
 GPIO_ROT_SW = 17
+
