66c59caf3fbed5a82c8910fa72b288d8b1ccebb1
MaxTechCode <maxtech@t-online.de>
2021-05-03 13:33:57 +0200

    added take_picture to measure.py

--- measure.py
+++ measure.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from sensors.DHT22 import measure_temp_ahum
 from sensors.adconverter import measure_ground_humidity, measure_light_level
+from sensors.camera import take_picture
 from server.enter_data import enter_measurement
 
 # this script needs to be called by a cron job to take measurements
@@ -12,3 +13,4 @@ light = measure_light_level()
 temp, ahum = measure_temp_ahum()
 fill = 100 # replace in future
 enter_measurement(temp, ahum, ghum, light, fill)
+take_picture()
