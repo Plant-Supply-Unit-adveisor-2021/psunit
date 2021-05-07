729d9f5a139ac5bb6413e878134a1f1f16ac10fa
MaxTechCode <maxtech@t-online.de>
2021-05-07 16:10:37 +0200

    reinforced DHT22 considering errors

--- sensors/DHT22.py
+++ sensors/DHT22.py
@@ -1,14 +1,25 @@
-import Adafruit_DHT
-from settings import GPIO_DHT22
-
-
-DHT_SENSOR = Adafruit_DHT.DHT22
+from Adafruit_DHT import DHT22, read_retry
+from settings import MEASURE_CONFIG, GPIO_DHT22
 
 def measure_temp_ahum():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_DHT22)
+    """
+    function with measures temperature and air humidity via the DHT22
+    returns: tuple (temp, a_hum) and (None, None) if something failed
+    """
+    try:
+        if not MEASURE_CONFIG['DHT22']:
+            # DHT22 not enabled in measure.config.json
+            print('DHT22 not activated in measure.config.json')
+            return (None, None)
+    
+        humidity, temperature = read_retry(DHT22, GPIO_DHT22)
 
-    if humidity is not None and temperature is not None:
-        return (temperature, humidity)
-    else:
-        print("Failed to retrieve data from humidity sensor")
+        if humidity is not None and temperature is not None:
+            return (temperature, humidity)
+        else:
+            print("DHT22: Failed to retrieve data from temp and humidity sensor")
+            return (None, None)
+    except Exception as e:
+        print("DHT22: Failed to retrieve data from temp and humidity sensor due to error")
+        print(e)
         return (None, None)
