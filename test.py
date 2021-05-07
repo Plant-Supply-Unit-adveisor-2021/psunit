09d6ffd59852be67872192e91c065b5fec7208ee
MaxTechCode <maxtech@t-online.de>
2021-05-07 15:14:28 +0200

    started ading measure.conf

--- test.py
+++ test.py
@@ -4,8 +4,15 @@ from server.test import test as server_test
 from sensors.test import test as sensors_test
 from ui.test import test as ui_test
 
-while(True):
-    sensors_test()
-    sleep(5)
+from settings import MEASURE_CONFIG
+
+if MEASURE_CONFIG['DHT22']:
+    print('YEAH')
+else:
+    print('SAD')
+
+#while(True):
+#    sensors_test()
+#    sleep(5)
 # server_test()
 # ui_test()
