7e12022a4c76f652a34ea7b95af07db2192b3efa
MaxTechCode <maxtech@t-online.de>
2021-05-24 10:56:59 +0200

    resolved auth conflict

--- push.py
+++ push.py
@@ -1,12 +1,14 @@
 from server.interface import push_data, push_images
 from datetime import datetime
+from time import sleep
 
 """
 
  python script to push data and images to the server
 
 """
-
+# sleep to not interfere with water.py
+sleep(20)
 print(datetime.now().strftime("Starting push.py at %d.%m.%Y %H:%M:%S"))
 push_data()
 push_images()
